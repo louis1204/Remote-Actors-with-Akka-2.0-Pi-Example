7c9e421c0548672be2fdbb4e1bf2a0d9c188155c
Louis <Louis@ZiftIts-MacBook-Pro.local>
2012-08-17 10:07:12 -0700

    Added custom dispatcher and reduced the mapping code.

--- app/controllers/Pi.scala
+++ app/controllers/Pi.scala
@@ -16,11 +16,12 @@ import views._
 import models._
 import com.typesafe.config.ConfigFactory
 import akka.actor.{ ActorRef, Props, Actor, ActorSystem }
+import akka.routing.RoundRobinRouter
 
 object Pi extends Controller {
   // Global Variables
   val RemoteSystem = ActorSystem("RemoteCreation", ConfigFactory.load.getConfig("remotecreation")) // Actor System
-  val worker = RemoteSystem.actorOf(Props[Worker], "Worker") // Worker Actor
+  val worker = RemoteSystem.actorOf(Props[Worker].withDispatcher("my-thread-pool-dispatcher").withRouter(RoundRobinRouter(5)), "Worker") // Worker Actor with custom dispatcher and router	
 
   // form for options
   val optionsForm: Form[Options] = Form(
@@ -28,16 +29,16 @@ object Pi extends Controller {
     mapping(
       "numWorkers" -> of[Int],
       "numElements" -> of[Int],
-      "numMessages" -> of[Int]) // The mapping signature doesn't match the User case class signature,
+      "numMessages" -> of[Int])(Options.apply)(Options.unapply)) // The mapping signature doesn't match the User case class signature,
       // so we have to define custom binding/unbinding functions
-      {
+/*      {
         // Binding: Create a Options from the mapping result			
         (numWorkers, numElements, NumMessages) => Options(numWorkers, numElements, NumMessages)
       } {
         // Unbinding: Create the mapping values from an existing Options value
         options => Some(options.numWorkers, options.numElements, options.numMessages)
       })
-
+*/
   // defines the action after the user hitting the submit button
   def submit = Action { implicit request =>
     optionsForm.bindFromRequest.fold(
@@ -49,7 +50,6 @@ object Pi extends Controller {
 
         // create a master, passing in the values from the 
         val master = RemoteSystem.actorOf(Props(new Master(options.numWorkers.toInt, options.numElements.toInt, options.numMessages.toInt)))
-
         master ! (Calculate) // send the message to master to calculate
         Ok(html.form(this.optionsForm)) // reload the form
       })
